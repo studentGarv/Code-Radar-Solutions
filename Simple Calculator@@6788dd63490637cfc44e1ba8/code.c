90040a33cc20babd50cbb3bb099c19d26e63c8ef
Garv Mehra <garv1630.becse24@chitkara.edu.in>
2025-02-12 12:54:36 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -7,13 +7,13 @@ int main (){
     if (c == '+'){
         printf("%d",a + b);
     }
-//     if (c == '-'){
-//         printf("%d",a - b);
-//     }
-//     if (c == ""){
-//         printf("%d",a * b);
-//     }
-//     if (c == "/"){
-//         printf("%d",a / b);
-//     }
+    if (c == '-'){
+        printf("%d",a - b);
+    }
+    if (c == ""){
+        printf("%d",a * b);
+    }
+    // if (c == "/"){
+    //     printf("%d",a / b);
+    // }
 }
